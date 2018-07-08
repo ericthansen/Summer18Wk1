35c541c697edefe9640dc355c9cbeef5ddc5f4cc
Eric H <20998658+ericthansen@users.noreply.github.com>
2018-07-08 10:11:54 -0500

    more advanced choice view - i.e. now can add multiple choices when adding a question

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [ 
         (None,        {'fields':['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
 
 # Register your models here.
